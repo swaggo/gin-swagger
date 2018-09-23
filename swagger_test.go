c669c09a224132159867d1bb58d8cd02c9646423
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2018-09-23 23:26:28 +0300

    Add disabling handler
    Add tests to disabling handler
    
    Cover 100% of disabling handler code

--- swagger_test.go
+++ swagger_test.go
@@ -2,6 +2,7 @@ package ginSwagger
 
 import (
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -28,7 +29,43 @@ func TestWrapHandler(t *testing.T) {
 
 	w4 := performRequest("GET", "/notfound", router)
 	assert.Equal(t, 404, w4.Code)
+}
 
+func TestDisablingWrapHandler(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.New()
+	disablingKey := "SWAGGER_DISABLE"
+
+	router.GET("/simple/*any", DisablingWrapHandler(swaggerFiles.Handler, disablingKey))
+
+	w1 := performRequest("GET", "/simple/index.html", router)
+	assert.Equal(t, 200, w1.Code)
+
+	w2 := performRequest("GET", "/simple/doc.json", router)
+	assert.Equal(t, 200, w2.Code)
+
+	w3 := performRequest("GET", "/simple/favicon-16x16.png", router)
+	assert.Equal(t, 200, w3.Code)
+
+	w4 := performRequest("GET", "/simple/notfound", router)
+	assert.Equal(t, 404, w4.Code)
+
+	os.Setenv(disablingKey, "true")
+
+	router.GET("/disabling/*any", DisablingWrapHandler(swaggerFiles.Handler, disablingKey))
+
+	w11 := performRequest("GET", "/disabling/index.html", router)
+	assert.Equal(t, 404, w11.Code)
+
+	w22 := performRequest("GET", "/disabling/doc.json", router)
+	assert.Equal(t, 404, w22.Code)
+
+	w33 := performRequest("GET", "/disabling/favicon-16x16.png", router)
+	assert.Equal(t, 404, w33.Code)
+
+	w44 := performRequest("GET", "/disabling/notfound", router)
+	assert.Equal(t, 404, w44.Code)
 }
 
 func performRequest(method, target string, router *gin.Engine) *httptest.ResponseRecorder {
