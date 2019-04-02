2d36fce153942f8b0a60144bd502b673f8c76a95
hakjosef <josef.hak@firma.seznam.cz>
2019-04-02 17:58:58 +0200

    added test using gin-contrib/gzip middleware
    checking correct Content-Type response header

--- swagger_test.go
+++ swagger_test.go
@@ -1,6 +1,7 @@
 package ginSwagger
 
 import (
+	"github.com/gin-contrib/gzip"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -95,6 +96,31 @@ func TestDisablingCustomWrapHandler(t *testing.T) {
 
 	w11 := performRequest("GET", "/disabling/index.html", router)
 	assert.Equal(t, 404, w11.Code)
+}
+
+func TestWithGzipMiddleware(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	router.Use(gzip.Gzip(gzip.BestSpeed))
+
+	router.GET("/*any", WrapHandler(swaggerFiles.Handler))
+
+	w1 := performRequest("GET", "/index.html", router)
+	assert.Equal(t, 200, w1.Code)
+	assert.Equal(t, w1.Header()["Content-Type"][0], "text/html; charset=utf-8")
+
+	w2 := performRequest("GET", "/swagger-ui.css", router)
+	assert.Equal(t, 200, w2.Code)
+	assert.Equal(t, w2.Header()["Content-Type"][0], "text/css; charset=utf-8")
+
+	w3 := performRequest("GET", "/swagger-ui-bundle.js", router)
+	assert.Equal(t, 200, w3.Code)
+	assert.Equal(t, w3.Header()["Content-Type"][0], "application/javascript")
+
+	w4 := performRequest("GET", "/doc.json", router)
+	assert.Equal(t, 200, w4.Code)
+	assert.Equal(t, w4.Header()["Content-Type"][0], "application/json")
 }
 
 func performRequest(method, target string, router *gin.Engine) *httptest.ResponseRecorder {
